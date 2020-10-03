22d3a07e17b2476079a21152dcde01dc380f72f5
iinoten <nteten0313@icloud.com>
2020-10-04 05:20:38 +0900

    アプリ内の表現を変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp()); //関数実行をワンライナーで書けるシンタックスシュガー
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //StatelessWidgetを継承
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) { //build でウィジェットのUIを作成
+    return MaterialApp( // MaterialDesignを使用(Flutterでは豊富なマテリアルデザインのウィジェットが提供されている)
       title: 'Welcome to Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar( // アプリケーションバーを表現
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Welcome to Flutter'), //「Center」で中央配置にし「Text」で文字列を配置
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
